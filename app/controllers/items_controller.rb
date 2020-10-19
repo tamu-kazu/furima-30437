18b1e73aa70e522886a1b78cac6cf30bcfc9c98d
tamu-kazu <71427147+tamu-kazu@users.noreply.github.com>
2020-10-19 12:16:07 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+  
 end
+
