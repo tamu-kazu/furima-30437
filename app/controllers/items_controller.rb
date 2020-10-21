243f048938b1c9bfe220c9138d11b312b1afe5d0
tamu-kazu <71427147+tamu-kazu@users.noreply.github.com>
2020-10-21 18:43:20 +0900

    削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,14 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    end
+  end
+
+
   private
 
   def set_item
