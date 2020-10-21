88f4aeef8a96e413daf4a47b0378136eb86393c5
tamu-kazu <71427147+tamu-kazu@users.noreply.github.com>
2020-10-21 18:46:14 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,11 +36,8 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if item.destroy
-      redirect_to root_path
-    end
+    redirect_to root_path if item.destroy
   end
-
 
   private
 
