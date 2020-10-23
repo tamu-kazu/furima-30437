95960930f150541a263e29d0517e7ed6fd63d4f6
tamu-kazu <71427147+tamu-kazu@users.noreply.github.com>
2020-10-23 14:47:34 +0900

    購入画面への遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,21 @@
 class OrdersController < ApplicationController
   def index
+    @user_order = UserOrder.new
   end
+
+  def create
+    @user_order = UserOrder.new(order_params)
+     if @user_order.valid?
+       @user_order.save
+       redirect_to action: :index
+     else
+       render action: :new
+     end
+  end
+
+  private
+ def order_params
+  params.require(:user_order).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id, :user_id, :item_id)
+ end
 
 end
