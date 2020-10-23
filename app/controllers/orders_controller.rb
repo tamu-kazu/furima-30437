b1cd9c071ec6c388fb8a4ac1f634c4b16a6d1344
tamu-kazu <71427147+tamu-kazu@users.noreply.github.com>
2020-10-23 19:15:17 +0900

    保存機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,27 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
     @user_order = UserOrder.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @user_order = UserOrder.new(order_params)
      if @user_order.valid?
-       @user_order.save
+       @user_order.save 
        redirect_to action: :index
      else
-       render action: :new
-     end
+       render action: :index
+    end
   end
 
   private
  def order_params
-  params.require(:user_order).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id, :user_id, :item_id)
+  params.require(:user_order).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id).merge(user_id: current_user.id, item_id: params[:item_id])
+ end
+
+ def set_item
+  @item = Item.find(params[:item_id])
  end
 
 end
