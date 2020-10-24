c6a1660a61ef7f2a4c5c34742f2f929734fa59c9
tamu-kazu <71427147+tamu-kazu@users.noreply.github.com>
2020-10-24 11:31:24 +0900

    クレジットカード決済機能の導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,8 +8,9 @@ class OrdersController < ApplicationController
   def create
     @user_order = UserOrder.new(order_params)
      if @user_order.valid?
+      pay_item
        @user_order.save 
-       redirect_to action: :index
+       redirect_to root_path
      else
        render action: :index
     end
@@ -17,11 +18,20 @@ class OrdersController < ApplicationController
 
   private
  def order_params
-  params.require(:user_order).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id).merge(user_id: current_user.id, item_id: params[:item_id])
+  params.require(:user_order).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
  end
 
  def set_item
   @item = Item.find(params[:item_id])
+ end
+
+ def pay_item
+  Payjp.api_key = "sk_test_8c3fc5bae0b4d0aedab7a5ed"
+  Payjp::Charge.create(
+    amount: @item.price,
+    card: order_params[:token], 
+    currency: 'jpy' 
+  )
  end
 
 end
--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,6 +1,6 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id, :user_id, :item_id, :token
 
  with_options presence: true do
   validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
