bbc09d4539f2c05dd7f1d1c66e9403b094f7e44c
tamu-kazu <71427147+tamu-kazu@users.noreply.github.com>
2020-10-25 09:26:09 +0900

    テストコード修正

--- spec/factories/user_orders.rb
+++ spec/factories/user_orders.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     prefecture_id { 2 }
     municipality { '大阪市' }
     address { '1-1' }
+    building_name { '柳ビル' }
     phone_number { '09012345678' }
     token { 'tok_abcdefghijk00000000000000000' }
   end
--- spec/models/user_order_spec.rb
+++ spec/models/user_order_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe UserOrder, type: :model do
     it 'すべての値が正しく入力されていれば保存できること' do
       expect(@user_order).to be_valid
     end
+    it 'building_nameが空でも保存できること' do
+      @user_order.building_name = nil
+      expect(@user_order).to be_valid
+    end
     it 'postal_codeが空だと保存できないこと' do
       @user_order.postal_code = nil
       @user_order.valid?
